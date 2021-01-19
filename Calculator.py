6ef63b149d2f80fd4b2cc22edf2644a2aa099b26
Moeed <ppro78741@gmail.com>
2021-01-19 22:45:07 +0330

    Added a couple of buttons

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,19 @@
-Calculator.py
+import kivy
+from kivy.app import App 
+from kivy.uix.widget import Widget 
+from kivy.properties import ObjectProperty
+from kivy.lang.builder import Builder
+
+Builder.load_file("Design.kv")
+
+class application(Widget):
+	pass
+
+class Calculator(App):
+	def build(self):
+		return application()
+
+
+
+if __name__ == "__main__":
+		Calculator().run()
